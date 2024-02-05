73b31e0e2a37b7daa9cfdd7e63ee22c278239d75
Mohammed <saadisaadi768@gmail.com>
2024-02-05 16:50:24 -0600

    working on reactions

--- components/cursor/Cursor.tsx
+++ components/cursor/Cursor.tsx
@@ -13,7 +13,11 @@ const Cursor = ({ color, x, y, message}: Props) => {
     translateY(${y}px)`}}>
      <CursorSVG color={color} />
 
-     
+     {message && (
+        <div className="absolute left-2 top-5 rounded-3xl px-4 py-2" style={{ backgroundColor: color}}>
+            <p className="whitespace-nowrap text-sm leading-relaxed">{message}</p>
+        </div>
+     )}
     </div>
   )
 }
