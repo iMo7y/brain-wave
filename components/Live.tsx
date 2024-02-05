bd5ba741abf51fad42ca6e17df7f44e5beacf7d0
Mohammed <saadisaadi768@gmail.com>
2024-02-05 16:13:31 -0600

    working on chat

---
+++ components/Live.tsx
@@ -0,0 +1,62 @@
+import { useMyPresence, useOthers } from "@/liveblocks.config"
+import LiveCursors from "./cursor/LiveCursors"
+import { useCallback, useState } from "react";
+import CursorChat from "./cursor/CursorChat";
+import { CursorMode } from "@/types/type";
+
+const Live = () => {
+  const others = useOthers();
+  const [{ cursor }, updateMyPresence] = useMyPresence() as any;
+
+  const [cursorState, setCursorState] = useState({
+    mode: CursorMode.Hidden,
+  })
+
+  const handlePointerMove = useCallback((event: React.PointerEvent ) => {
+    event.preventDefault();
+
+    const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
+    const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
+
+    updateMyPresence({ cursor: { x, y } });
+  }, [])
+
+  const handlePointerLeave = useCallback((event: React.PointerEvent ) => {
+    setCursorState({ mode: CursorMode.Hidden })
+
+
+    updateMyPresence({ cursor: null, message: null });
+  }, [])
+
+  const handlePointerDown = useCallback((event: React.PointerEvent ) => {
+    event.preventDefault();
+
+    const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
+    const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
+
+    updateMyPresence({ cursor: { x, y } });
+  }, [])
+
+  return (
+    <div
+      onPointerMove={handlePointerMove}
+      onPointerLeave={handlePointerLeave}
+      onPointerDown={handlePointerDown}
+      className=" h-[100vh] w-full flex justify-center items-center text-center"
+    >
+        <h1 className="text-2xl">Brain Wave</h1>
+
+        {cursor && (
+            <CursorChat 
+            cursor={cursor}
+            cursorState={cursorState}
+            setCursorState={setCursorState}
+            updateMyPresence={updateMyPresence}
+            />
+        )}
+        <LiveCursors others={others} />
+    </div>
+  )
+}
+
+export default Live
